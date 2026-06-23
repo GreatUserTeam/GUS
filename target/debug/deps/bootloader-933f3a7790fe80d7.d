d63930e9322b21fc82b02ac49d4921e8dc947583
zvenios <zvenios.rs@gmail.com>
2026-06-23 16:14:40 +0300

    Update edition to 2024

---
+++ target/debug/deps/bootloader-933f3a7790fe80d7.d
@@ -0,0 +1,7 @@
+/home/zvenios/GUS/target/debug/deps/bootloader-933f3a7790fe80d7.d: /home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs /home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs /home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs
+
+/home/zvenios/GUS/target/debug/deps/libbootloader-933f3a7790fe80d7.rmeta: /home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs /home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs /home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs
+
+/home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs:
+/home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs:
+/home/zvenios/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs:
---
+++ target/debug/deps/gus-8e50cf0d37406944.d
@@ -0,0 +1,7 @@
+/home/zvenios/GUS/target/debug/deps/gus-8e50cf0d37406944.d: src/main.rs src/vga_buffer.rs src/keyboard.rs
+
+/home/zvenios/GUS/target/debug/deps/libgus-8e50cf0d37406944.rmeta: src/main.rs src/vga_buffer.rs src/keyboard.rs
+
+src/main.rs:
+src/vga_buffer.rs:
+src/keyboard.rs:
